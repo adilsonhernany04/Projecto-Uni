c38c7f5eec406246b6995e1ee56920e7c0d28943
adilsonhernany04 <adilsonaurelio2004@gmail.com>
2024-11-28 15:40:57 +0100

    Adicionei as funcionalidades de: - Adicionar usuario - Remover usuario - Actualizar dados do usuario - Pesquisar usuario/s

--- src/Controles/ControleUsuario.java
+++ src/Controles/ControleUsuario.java
@@ -1,4 +1,3 @@
-
 package Controles;
 
 import Classes.Usuario;
@@ -6,10 +5,6 @@ import conectar.Conexao;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import javax.swing.table.DefaultTableModel;
-import View.UsuarioView;
-import com.mysql.cj.jdbc.PreparedStatementWrapper;
-import java.util.List;
 import java.util.ArrayList;
 import javax.swing.JOptionPane;
 
@@ -19,11 +14,16 @@ import javax.swing.JOptionPane;
  */
 public class ControleUsuario {
 
-    public void cadastrarUsuario(Usuario usuario) throws SQLException {
+    PreparedStatement pdec = null;
+    ResultSet rs = null;
+    ArrayList<Usuario> verLista = new ArrayList<>();
+
+    @SuppressWarnings("ConvertToTryWithResources")
+
+    // METPODO PARA CADASTRAR
+    public boolean cadastrarUsuario(Usuario usuario) {
 
         String cmdS = "INSERT INTO estudante VALUES (?, ?, ?, ? , ?)";
-
-        PreparedStatement pdec = null;
 
         try {
             pdec = Conexao.pegarConexao().prepareStatement(cmdS);
@@ -33,50 +33,146 @@ public class ControleUsuario {
             pdec.setString(2, usuario.getNome()); // nome do estudante
             pdec.setString(3, usuario.getEmail()); // email do estudante
             pdec.setString(4, usuario.getSenha()); // endereco do estudante
-            pdec.setString(5, usuario.getEndereco()); // senha do estudante
-            
+            pdec.setString(5, usuario.getMorada()); // senha do estudante
+
             pdec.execute();
             pdec.close();
-            
+
+            return true;
+
         } catch (SQLException e) {
             e.printStackTrace();
-        } finally {
-            if (pdec != null) pdec.close();
-            if (Conexao.pegarConexao() != null) Conexao.close();
+            return false;
         }
-
     }
-    //
-    //
 
     //
-    public List<Usuario> Ver() {
+    //
+    // METODO PARA LISTAR/MOSTRARs OS CADASTRADOS
+    public ArrayList<Usuario> VerUsuarios() {
 
-        String cmdS = "SELECT id, nome, email, endereco FROM estudante";
+        String cmdS = "SELECT id, nome, email, endereco, senha FROM estudante";
 
-        List<Usuario> ver = new ArrayList<>();
-        
-        PreparedStatement pdec = null;
-        ResultSet rs = null;
-                
         try {
             pdec = (PreparedStatement) Conexao.pegarConexao().prepareStatement(cmdS);
             rs = pdec.executeQuery();
-            
-            while(rs.next()) {
+
+            while (rs.next()) {
+
                 Usuario us = new Usuario();
                 us.setId_usuario(rs.getInt("id"));
                 us.setNome(rs.getString("nome"));
-                us.setNome(rs.getString("email"));
-                us.setNome(rs.getString("endereco"));
-                
-                ver.add(us);            }
+                us.setEmail(rs.getString("email"));
+                us.setMorada(rs.getString("endereco"));
+                us.setSenha(rs.getString("senha"));
+
+                verLista.add(us);
+            }
+
         } catch (SQLException e) {
-            JOptionPane.showMessageDialog(null, "Erro!: "+ e.getMessage());
             e.printStackTrace();
-        } 
-        return ver;
+        }
+
+        return verLista;
     }
+
+    //
+    //
+    // METODO PARA ACTUALIZAR OS DADOS DE USUARIOS CADASTRADOS
+    public boolean actualizarDados(Usuario usuario) {
+
+        String cmdS = "UPDATE estudante SET nome = ?, email = ?,senha = ?, endereco = ? "
+                + "WHERE id = ?";
+
+        PreparedStatement pdec = null;
+
+        try {
+            pdec = Conexao.pegarConexao().prepareStatement(cmdS);
+
+            // Definir os valores dos parâmetros na instrução SQL
+            pdec.setString(1, usuario.getNome()); // nome do estudante
+            pdec.setString(2, usuario.getEmail()); // email do estudante
+            pdec.setString(3, usuario.getSenha()); // endereco do estudante
+            pdec.setString(4, usuario.getMorada()); // senha do estudante
+
+            // o id para seleccionar 
+            pdec.setInt(5, usuario.getId_usuario()); // id do estudante
+
+            pdec.execute();
+            pdec.close();
+
+            return true;
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return false;
+        }
+
+    }
+
+    //
+    //
+    // METODO PARA DELETAR USUARIO
+    public boolean apagarUsuarioPeloId(int id) {
+        String cmdS = "DELETE FROM estudante WHERE id = ? ";
+
+        PreparedStatement pdec = null;
+
+        try {
+            pdec = Conexao.pegarConexao().prepareStatement(cmdS);
+
+            // Definir o valore do parâmetro na instrução SQL
+            // o id para seleccionar 
+            pdec.setInt(1, id); // id do estudante
+
+            pdec.execute();
+            pdec.close();
+
+            return true;
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+    //
+    //
+    // METODO PARA PESQUISAR/BUSCAR USUARIO/s
+    public ArrayList<Usuario> buscarUsuarioPeloNome(String nome) {
         
+        String cmdS = "SELECT * FROM estudante WHERE nome LIKE ?"; // Consulta com LIKE para busca parcial
+        PreparedStatement pdec = null;
+        
+
+        try {
+            pdec = Conexao.pegarConexao().prepareStatement(cmdS);
+
+            // Definir o parâmetro com "%" para buscar por nomes semelhantes
+            // 
+            pdec.setString(1, nome + "%"); // pega usuario pela inicial
+
+            ResultSet rs = pdec.executeQuery();
+
+            // Processar os resultados da consulta
+            while (rs.next()) {
+                Usuario usuario = new Usuario();
+                usuario.setId_usuario(rs.getInt("id")); // Alterar o nome da coluna se necessário
+                usuario.setNome(rs.getString("nome"));
+                usuario.setEmail(rs.getString("email"));
+                usuario.setMorada(rs.getString("endereco"));
+                usuario.setSenha(rs.getString("senha"));
+
+                verLista.add(usuario);
+            }
+
+            rs.close();
+            pdec.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return verLista;
+    }
 
 }
